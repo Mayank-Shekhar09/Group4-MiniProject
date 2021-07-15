b8c1eefb6601146f7bfc958a87dc8c5c7da2b50e
RohiniNair99 <87111152+RohiniNair99@users.noreply.github.com>
2021-07-15 20:01:35 +0530

    Added test

---
+++ AddCourseBLTests/AddCourseTests.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AddCourseBL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddCourseBL.Tests
+{
+    [TestClass()]
+    public class AddCourseTests
+    {
+
+        [TestMethod()]
+        public void PSetLevelTest()
+        {
+            AddCourse a = new AddCourse();
+            string expected = "ILSWITTI500";
+            a.CourseId = "ILSWITTI";
+            string actual = a.SetLevel("L2");
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod()]
+        public void NSetLevelTest()
+        {
+            AddCourse a = new AddCourse();
+            string expected = "ILSWITTI500";
+            a.CourseId = "ILSWITTI";
+            string actual = a.SetLevel("L1");
+            Assert.AreNotEqual(expected, actual);
+
+        }
+
+        [TestMethod()]
+        public void PAddCourseTest()
+        {
+            string[] faculty = new string[] { "Ramesh", "Suresh" };
+            Mode mode = Mode.HandsOn;
+            AddCourse a = new AddCourse("IL", "SW", 'T', 'T', 'I', "Java", 8.0F, "Rajat",faculty,mode,"abc");
+            string expected = "ILSWTTI";
+            string actual = a.CourseId;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void NAddCourseTest()
+        {
+            string[] faculty = new string[] { "Ramesh", "Suresh" };
+            Mode mode = Mode.HandsOn;
+            AddCourse a = new AddCourse("IL", "SW", 'T', 'T', 'I', "Java", 8.0F, "Rajat", faculty, mode, "abc");
+            string expected = "ILSWTI";
+            string actual = a.CourseId;
+            Assert.AreNotEqual(expected, actual);
+        }
+    }
+}
